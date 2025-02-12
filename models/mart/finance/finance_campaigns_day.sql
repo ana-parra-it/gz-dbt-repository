e64710079b108ab7f1260ae47a61034d3ddcc86f
annami.parra@gmail.com <annami.parra@gmail.com>
2025-02-12 14:45:12 +0000

    finance_campaigns_month

--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -1,5 +1,19 @@
-SELECT *
+SELECT date_date
+,CAST((operational_margin-ads_cost) AS INT64) as ads_margin
+,average_basket
+,operational_margin
+,ads_cost
+,ads_impression
+,ads_clicks
+,quantity
+,revenue
+,purchase_cost
+,margin
+,shipping_fee
+,logcost
+,ship_cost
 FROM
 {{ ref('int_campaigns_day') }} as s
 JOIN {{ ref('finance_days') }} as p
 USING (date_date) 
+ORDER by date_date DESC
